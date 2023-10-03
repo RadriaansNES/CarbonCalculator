25d91b1fc0347282e583bd64c91ae668eab9a5bc
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-10-03 14:29:19 -0400

    Ready for calc css

--- Frontend/Components/Home/home.component.js
+++ Frontend/Components/Home/home.component.js
@@ -5,6 +5,17 @@ angular.module('myApp')
 
 function HomeController($state) {
   this.goToLogin = function () {
-    $state.go('login');
+    $state.go('layout.login');
   };
+
+  // Function to scroll to the top of the viewport
+  function scrollToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  // Attach an event listener to all links
+  const links = document.querySelectorAll('a, .scroll-top-button');
+  links.forEach(link => {
+    link.addEventListener('click', scrollToTop);
+  });
 }
