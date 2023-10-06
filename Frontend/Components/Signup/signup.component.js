61b28ff850239edfd968e05da8204275ea924956
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-10-06 09:20:22 -0400

    Pre module change.

--- Frontend/Components/Signup/signup.component.js
+++ Frontend/Components/Signup/signup.component.js
@@ -3,6 +3,19 @@
 angular.module('myApp')
   .controller('SignupController', SignupController);
 
-function SignupController() {
-  // Logic
+SignupController.$inject = ['$scope'];
+
+function SignupController($scope) {
+  $scope.formData = {}; // Initialize formData object to store form values
+
+  $scope.handleChange = function () {
+    // Handle form changes here
+    // For example, you can log the form data
+    console.log($scope.formData);
+  };
+
+  $scope.handleSubmit = function () {
+    // Handle form submission here, e.g., send the data to your backend server
+    // You can perform your registration logic here
+  };
 }
