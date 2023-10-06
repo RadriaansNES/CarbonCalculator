d775d9759af4b0f0d66e563050238ec9be77c782
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-10-06 13:31:29 -0400

    Various.

--- Frontend/Components/Signup/signup.component.js
+++ Frontend/Components/Signup/signup.component.js
@@ -3,19 +3,7 @@
 angular.module('myApp')
   .controller('SignupController', SignupController);
 
-SignupController.$inject = ['$scope'];
 
 function SignupController($scope) {
-  $scope.formData = {}; // Initialize formData object to store form values
 
-  $scope.handleChange = function () {
-    // Handle form changes here
-    // For example, you can log the form data
-    console.log($scope.formData);
-  };
-
-  $scope.handleSubmit = function () {
-    // Handle form submission here, e.g., send the data to your backend server
-    // You can perform your registration logic here
-  };
 }
