fc43a92e3761de4d5ba35f9dee7740b68cd11703
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-10-10 15:05:32 -0400

    Initialization of backend.

---
+++ Backend/src/main/java/com/carboncalc/onrender/backend/CarbonFootprintCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.carboncalc.onrender.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonFootprintCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonFootprintCalculatorApplication.class, args);
+	}
+
+}
