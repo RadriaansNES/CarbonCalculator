bc859ffa17f6c9d724b4d3590849037832c8bc67
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-10-12 07:25:39 -0400

    Fixed input render issues. Fixed refresh break. Additional dev tools

--- Backend/src/main/java/com/carboncalc/onrender/backend/controller/FrontendController.java
+++ Backend/src/main/java/com/carboncalc/onrender/backend/controller/FrontendController.java
@@ -4,11 +4,10 @@ package com.carboncalc.onrender.backend.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class FrontendController {
-    @GetMapping("/frontend")
-    public String frontend() {
-        return "forward:/frontend/index.html";
+    @GetMapping("/{path:[^\\.]*}")
+    public String redirect() {
+        return "index.html";
     }
 }
